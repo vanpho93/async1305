56952c36cf73c90853b56761df2bb27b7112728c
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-10 11:09:12 +0700

    chane

---
+++ e.js
@@ -0,0 +1,21 @@
+const add = (a, b) => (
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const condition = typeof a === 'number' && typeof b === 'number';
+            if (!condition) return reject(new Error('Type error!'));
+            resolve(a + b);
+        }, 1000);
+    })
+);
+
+add(4, 5)
+.then(tong => tong * 2)
+.then(a => a * 2)
+.then(b => add(b, '-30'))
+.catch(err => {
+    console.log(err);
+    return 1000;
+})
+.then(d => add(d, '-1'))
+.then(e => console.log(e))
+.catch(err2 => console.log('LOIIIII: ', err2));
